d762e1e0c66f6f0a4bf67cd2fc3e05ca5e54e495
David Deus <davidafonsodeus@outlook.com>
2022-11-16 13:12:02 +0000

    index and show

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,23 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: %i[show create]
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show; end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant.save
+    redirect_to restaurants_path(@restaurants)
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
 end
